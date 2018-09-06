4f23f55ec9f5b4c420cbdb7c4beed7851cfcee64
zeo7758 <17621063136@163.com>
2018-09-06 10:39:53 +0800

    init

---
+++ build/webpack.config.js
@@ -0,0 +1,91 @@
+const path=require('path');
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+module.exports={
+    entry:'./src/index.js',
+    output:{
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module:{
+        loaders: [
+            { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel' },
+            { test: /\.less$/, exclude: /node_modules/, loader: 'style!css!postcss!less' },
+            { test: /\.css$/, exclude: /node_modules/, loader: 'style!css!postcss' },
+            { test:/\.(png|gif|jpg|jpeg|bmp)$/i, loader:'url-loader?limit=5000' },  // 限制大小5kb
+            { test:/\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, loader:'url-loader?limit=5000'} // 限制大小小于5k
+        ]
+    },
+    postcss: [
+        require('autoprefixer') //调用autoprefixer插件，例如 display: flex
+    ],
+    plugins: [
+        // new webpack.optimize.UglifyJsPlugin(),
+
+        // html 模板插件
+        new HtmlWebpackPlugin({template: './src/template.html'}),
+        // 热加载插件
+        new webpack.HotModuleReplacementPlugin(),
+
+        // 打开浏览器
+        new OpenBrowserPlugin({
+          url: 'http://localhost:8080'
+        }),
+
+        // 可在业务 js 代码中使用 __DEV__ 判断是否是dev模式（dev模式下可以提示错误、测试报告等, production模式不提示）
+        new webpack.DefinePlugin({
+          __DEV__: JSON.stringify(JSON.parse((process.env.NODE_ENV == 'dev') || 'false'))
+        //   "process.env": {
+        //        NODE_ENV: JSON.stringify("production")
+        //      }
+        })
+    ],
+    devServer: {
+        proxy: {
+          // 凡是 `/api` 开头的 http 请求，都会被代理到 localhost:3000 上，由 koa 提供 mock 数据。
+          // koa 代码在 ./mock 目录中，启动命令为 npm run mock
+          '/api': {
+            target: 'http://recommender-api-ms.juejin.im',
+            secure: false,
+             "changeOrigin":true,
+             pathRewrite: {
+                 '^/api': ''
+             }
+         },
+         '/xiaoce-api': {
+             target: 'https://xiaoce-timeline-api-ms.juejin.im',
+             secure: false,
+              "changeOrigin":true,
+              pathRewrite: {
+                  '^/xiaoce-api': ''
+              }
+         },
+         '/xiaoceinfo-api': {
+           
+             target: 'https://xiaoce-cache-api-ms.juejin.im',
+             secure: false,
+              "changeOrigin":true,
+              pathRewrite: {
+                  '^/xiaoceinfo-api': ''
+              }
+         },
+         '/article-api': {
+           
+            target: 'https://post-storage-api-ms.juejin.im',
+            secure: false,
+             "changeOrigin":true,
+             pathRewrite: {
+                 '^/article-api': ''
+             }
+        }
+         
+        },
+        contentBase: "./src/common", //本地服务器所加载的页面所在的目录
+        colors: true, //终端中输出结果为彩色
+        historyApiFallback: true, //不跳转
+        inline: true, //实时刷新
+        hot: true  // 使用热加载插件 HotModuleReplacementPlugin
+    }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+## web using react
